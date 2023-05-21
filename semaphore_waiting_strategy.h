e3cdced25d19cb03190fa306ef4c34c17431df7c
theartful <101147828+theartful@users.noreply.github.com>
2023-05-21 21:41:12 +0300

    support macos (#2)
    * support macos
    
    * update README.md

--- semaphore_waiting_strategy.h
+++ semaphore_waiting_strategy.h
@@ -13,9 +13,11 @@
 #include <time.h>
 #elif defined(_WIN32) || defined(__MSYS__)
 #include <windows.h>
+#elif defined(__MACH__)
+#include <mach/mach_init.h> // for mach_task_self
+#include <mach/semaphore.h> // for the rest of the semaphore interface
+#include <mach/task.h>      // for semaphore_create and semaphore_destroy
 #endif
-
-#include "broadcast_queue.h"
 
 namespace broadcast_queue {
 
@@ -28,7 +30,9 @@ public:
 
   void release(int val = 1) {
     for (int i = 0; i < val; i++) {
-      sem_post(&m_semaphore);
+      if (sem_post(&m_semaphore) != 0) {
+        throw std::system_error(errno, std::system_category());
+      }
     }
   }
 
@@ -113,8 +117,8 @@ public:
   ~semaphore() { CloseHandle(m_semaphore); }
 
   void release(int val = 1) {
-    ReleaseSemaphore(m_semaphore, val, NULL);
     m_semaphore_value.fetch_add(val, std::memory_order_relaxed);
+    ReleaseSemaphore(m_semaphore, val, NULL);
   }
 
   void acquire() {
@@ -176,7 +180,116 @@ private:
   std::atomic<uint32_t> m_semaphore_value;
 };
 #elif defined(__MACH__)
-// TODO
+class semaphore {
+public:
+  semaphore(int initial_value = 0) {
+    kern_return_t result = semaphore_create(mach_task_self(), &m_semaphore,
+                                            SYNC_POLICY_FIFO, initial_value);
+    switch (result) {
+    case KERN_SUCCESS:
+      // hooray
+      break;
+    case KERN_INVALID_ARGUMENT:
+      throw std::logic_error("semaphore_create invalid argument!");
+      break;
+    case KERN_RESOURCE_SHORTAGE:
+      throw std::runtime_error("semaphore_create resource shortage!");
+      break;
+    default:
+      throw std::runtime_error("semaphore_create unknown error!");
+      break;
+    }
+  }
+
+  ~semaphore() { semaphore_destroy(mach_task_self(), m_semaphore); }
+
+  void release(int val = 1) {
+    m_semaphore_value.fetch_add(val, std::memory_order_relaxed);
+    for (int i = 0; i < val; i++) {
+      switch (semaphore_signal(m_semaphore)) {
+      case KERN_SUCCESS:
+        // hooray
+        break;
+      case KERN_INVALID_ARGUMENT:
+        throw std::logic_error("semaphore_signal invalid argument!");
+        break;
+      case KERN_TERMINATED:
+        throw std::runtime_error(
+            "semaphore_signal semaphore has been terminated!");
+        break;
+      }
+    }
+  }
+
+  void acquire() {
+    kern_return_t result;
+    while ((result = semaphore_wait(m_semaphore)) != KERN_SUCCESS) {
+      switch (result) {
+      case KERN_TERMINATED:
+        throw std::runtime_error(
+            "semaphore_signal semaphore has been terminated!");
+        break;
+      case KERN_INVALID_ARGUMENT:
+        throw std::logic_error("semaphore_signal invalid argument!");
+        break;
+      case KERN_ABORTED:
+        // spurious wake
+        break;
+      }
+    }
+    m_semaphore_value.fetch_sub(1, std::memory_order_relaxed);
+  }
+
+  bool try_acquire() { return try_acquire_for(std::chrono::seconds(0)); }
+
+  template <typename Rep, typename Period>
+  bool try_acquire_for(const std::chrono::duration<Rep, Period> &timeout) {
+    mach_timespec_t timeout_spec;
+
+    auto secs = std::chrono::duration_cast<std::chrono::seconds>(timeout);
+
+    timeout_spec.tv_sec = secs.count();
+    timeout_spec.tv_nsec =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(timeout - secs)
+            .count();
+
+    switch (semaphore_timedwait(m_semaphore, timeout_spec)) {
+    case KERN_SUCCESS:
+      // hooray
+      m_semaphore_value.fetch_sub(1, std::memory_order_relaxed);
+      return true;
+    case KERN_TERMINATED:
+      throw std::runtime_error(
+          "semaphore_signal semaphore has been terminated!");
+      return false;
+    case KERN_INVALID_ARGUMENT:
+      throw std::logic_error("semaphore_signal invalid argument!");
+      return false;
+    case KERN_INVALID_VALUE:
+      throw std::logic_error("semaphore_signal invalid time value!");
+      return false;
+    case KERN_ABORTED: // spurious wake
+    case KERN_OPERATION_TIMED_OUT:
+    default:
+      return false;
+    }
+  }
+
+  template <typename Clock, typename Duration>
+  bool
+  try_acquire_until(const std::chrono::time_point<Clock, Duration> &until) {
+    auto now = Clock::now();
+    if (now > until)
+      return false;
+    return try_acquire_for(until - now);
+  }
+
+  int value() { return m_semaphore_value.load(std::memory_order_relaxed); }
+
+private:
+  semaphore_t m_semaphore;
+  std::atomic<uint32_t> m_semaphore_value;
+};
 #endif
 
 class semaphore_waiting_strategy {
